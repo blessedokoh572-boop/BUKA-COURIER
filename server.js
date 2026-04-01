fff7a22cb4babdb480b5d8b6b34a999c65b34d87
blessedokoh572-boop <blessedokoh572@gmail.com>
2026-04-01 13:09:35 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,21 +1,34 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 
+// MIDDLEWARE FIRST
+app.use(cors());
 app.use(express.json());
 
 let shipments = [];
 
+// Create shipment
 app.post("/ship", (req, res) => {
   shipments.push(req.body);
   res.send({ message: "Shipment saved" });
 });
 
-app.getfetch('https://buka-courier.onrender.com/api/track') => {
+// Track shipment
+app.get("/api/track/:id", (req, res) => {
   let shipment = shipments.find(s => s.trackingId === req.params.id);
   res.json(shipment || null);
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+// Home route
+app.get("/", (req, res) => {
+  res.send("Buka Courier API is running ");
+});
 
-const cors = require("cors");
-app.use(cors());
+// USE RENDER PORT
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
