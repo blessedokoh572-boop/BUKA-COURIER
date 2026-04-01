19c2eedf2f3e3bf3a46ebb45a38873eaa0cdb1bf
blessedokoh572-boop <blessedokoh572@gmail.com>
2026-04-01 11:44:45 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+let shipments = [];
+
+app.post("/ship", (req, res) => {
+  shipments.push(req.body);
+  res.send({ message: "Shipment saved" });
+});
+
+app.get("/track/:id", (req, res) => {
+  let shipment = shipments.find(s => s.trackingId === req.params.id);
+  res.json(shipment || null);
+});
+
+app.listen(3000, () => console.log("Server running on port 3000"));
